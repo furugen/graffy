741f7bf27e4df7afe406c92d64eb68c318859501
Yuu Furugen <furugen098@gmail.com>
2018-05-02 18:01:09 +0900

    jquery-uiのコンポーネントを更新する。

--- src/app/shared/chart-list/chart-list.component.ts
+++ src/app/shared/chart-list/chart-list.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-chart-list',
-  templateUrl: './chart-list.component.html',
-  styleUrls: ['./chart-list.component.css']
+  selector: "app-chart-list",
+  templateUrl: "./chart-list.component.html",
+  styleUrls: ["./chart-list.component.css"]
 })
 export class ChartListComponent implements OnInit {
-
   private data: any;
   chartOptions: IgDataChart;
   title: string;
@@ -14,36 +13,74 @@ export class ChartListComponent implements OnInit {
 
   constructor() {
     this.title = "備品";
-    this.data = [{ "CountryName": "China", "Pop1995": 1216, "Pop2005": 1297, "Pop2015": 1361, "Pop2025": 1394 }, { "CountryName": "India", "Pop1995": 920, "Pop2005": 1090, "Pop2015": 1251, "Pop2025": 1396 }, { "CountryName": "United States", "Pop1995": 266, "Pop2005": 295, "Pop2015": 322, "Pop2025": 351 }, { "CountryName": "Indonesia", "Pop1995": 197, "Pop2005": 229, "Pop2015": 256, "Pop2025": 277 }, { "CountryName": "Brazil", "Pop1995": 161, "Pop2005": 186, "Pop2015": 204, "Pop2025": 218 }];
+    this.data = [
+      {
+        CountryName: "China",
+        Pop1995: 1216,
+        Pop2005: 1297,
+        Pop2015: 1361,
+        Pop2025: 1394
+      },
+      {
+        CountryName: "India",
+        Pop1995: 920,
+        Pop2005: 1090,
+        Pop2015: 1251,
+        Pop2025: 1396
+      },
+      {
+        CountryName: "United States",
+        Pop1995: 266,
+        Pop2005: 295,
+        Pop2015: 322,
+        Pop2025: 351
+      },
+      {
+        CountryName: "Indonesia",
+        Pop1995: 197,
+        Pop2005: 229,
+        Pop2015: 256,
+        Pop2025: 277
+      },
+      {
+        CountryName: "Brazil",
+        Pop1995: 161,
+        Pop2005: 186,
+        Pop2015: 204,
+        Pop2025: 218
+      }
+    ];
     this.chartTitles = ["A", "B", "C", "D", "F", "G", "H", "I"];
 
     this.chartOptions = {
       width: "200",
       height: "200",
       dataSource: this.data,
-      axes: [{
-        name: "NameAxis",
-        type: "categoryX",
-        label: "CountryName"
-      },
-      {
-        name: "PopulationAxis",
-        type: "numericY",
-        minimumvalue: 0
-      }],
-      series: [{
-        name: "2015Population",
-        type: "column",
-        isHighlightingEnabled: true,
-        isTransitionInEnabled: true,
-        xAxis: "NameAxis",
-        yAxis: "PopulationAxis",
-        valueMemberPath: "Pop2015"
-      }]
-    }
-  }  
-
-  ngOnInit() {
+      axes: [
+        {
+          name: "NameAxis",
+          type: "categoryX",
+          label: "CountryName"
+        },
+        {
+          name: "PopulationAxis",
+          type: "numericY",
+          minimumvalue: 0
+        }
+      ],
+      series: [
+        {
+          name: "2015Population",
+          type: "column",
+          isHighlightingEnabled: true,
+          isTransitionInEnabled: true,
+          xAxis: "NameAxis",
+          yAxis: "PopulationAxis",
+          valueMemberPath: "Pop2015"
+        }
+      ]
+    };
   }
 
+  ngOnInit() {}
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,24 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ChartListComponent } from './chart-list/chart-list.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ChartListComponent } from "./chart-list/chart-list.component";
 
-import { IgniteUIModule } from 'igniteui-angular-wrappers';
+import { IgniteUIModule } from "igniteui-angular-wrappers";
 
-import { IgxCardModule } from 'igniteui-js-blocks/main';
-
-
+import { IgxCardModule } from "igniteui-angular/main";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    IgxCardModule,
-    IgniteUIModule
-  ],
-  exports: [
-    ChartListComponent,
-    IgxCardModule,
-    IgniteUIModule,
-  ],
+  imports: [CommonModule, IgxCardModule, IgniteUIModule],
+  exports: [ChartListComponent, IgxCardModule, IgniteUIModule],
   declarations: [ChartListComponent]
 })
-export class SharedModule { }
+export class SharedModule {}
